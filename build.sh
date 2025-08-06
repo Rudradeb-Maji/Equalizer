4c9a730a5fc5298de7c9bcbab0c307d510a54b65
Rudradeb-Maji <rudradebmaji@gmail.com>
2025-08-06 19:03:41 +0530

    build.sh added

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Download yt-dlp to the project root
+curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o yt-dlp
+
+# Make it executable
+chmod +x yt-dlp
