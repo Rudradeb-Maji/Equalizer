2d9357e997a3f13be371217f53839dee099e8259
Rudradeb-Maji <rudradebmaji@gmail.com>
2025-08-06 18:57:23 +0530

    ytdlp default changed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const { Innertube, UniversalCache } = require("youtubei.js");
 const app = express();
 const cors = require("cors");
 const YTDlpWrap = require("yt-dlp-wrap").default; // ✅ Named import, with correct casing
-const ytdlp = new YTDlpWrap("./yt-dlp");
+const ytdlp = new YTDlpWrap();
 const PORT = 5000;
 const { Client } = require("youtubei.js");
 app.use(
